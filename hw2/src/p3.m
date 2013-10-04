f52631ccc0c3789dc7952970ee0f284d6e688842
willie neiswanger <willie.neiswanger@gmail.com>
2013-10-04 00:59:37 -0400

    Added hw2 files that i turned in, in hw2/doc/ and hw2/src/ dirs.

--- hw2/src/p3.m
+++ hw2/src/p3.m
@@ -1,54 +1,57 @@
-function [Bcell,cntVec,rmseVec_train,rmseVec_test,rBcell,rcntVec,subObj,mcObj] = p3(trainWs,testWs)
-    %Y = load(trainWs,'train');
-    %Yt = load(testWs,'test');
+function p3(trainWs,testWs)
+    Y = load(trainWs,'train');
+    Yt = load(testWs,'test');
 
-    %% part a
-    %lams = logspace(0,3,30);
-    %B = zeros(size(Y));
-    %for i=1:length(lams)
-        %[Bcell{i},cntVec(i)] = mc(B,Y,lams(i));
-        %fprintf('Finished part a, lam=%g, iterCnt=%d\n',lams(i),cntVec(i));
-    %end
+    % part a
+    lams = logspace(0,3,30);
+    B = zeros(size(Y));
+    for i=1:length(lams)
+        [Bcell{i},cntVec(i)] = mc(B,Y,lams(i));
+        fprintf('Finished part a, lam=%g, iterCnt=%d\n',lams(i),cntVec(i));
+    end
 
-    %% part b
-    %for i=1:length(Bcell)
-        %rmseVec_train(i) = rmse(Y,Bcell{i});
-        %rmseVec_test(i) = rmse(Yt,Bcell{i});
-    %end
+    % part b
+    for i=1:length(Bcell)
+        rmseVec_train(i) = rmse(Y,Bcell{i});
+        rmseVec_test(i) = rmse(Yt,Bcell{i});
+    end
 
-    %% part c
-    %rlams = fliplr(lams);
-    %[rBcell{1},rcntVec(1)] = mc(B,Y,rlams(1));
-    %fprintf('Finished part c, lam=%g, iterCnt=%d\n',rlams(1),rcntVec(1));
-    %for i=2:length(rlams)
-        %[rBcell{i},rcntVec(i)] = mc(rBcell{i-1},Y,rlams(i));
-        %fprintf('Finished part c, lam=%g, iterCnt=%d\n',rlams(i),rcntVec(i));
-    %end
-
-    load('TEST_RUN_02');
+    % part c
+    rlams = fliplr(lams);
+    [rBcell{1},rcntVec(1)] = mc(B,Y,rlams(1));
+    fprintf('Finished part c, lam=%g, iterCnt=%d\n',rlams(1),rcntVec(1));
+    for i=2:length(rlams)
+        [rBcell{i},rcntVec(i)] = mc(rBcell{i-1},Y,rlams(i));
+        fprintf('Finished part c, lam=%g, iterCnt=%d\n',rlams(i),rcntVec(i));
+    end
 
     % part d
     slams = [1,5,10];
     B = zeros(size(Y));
     for i=1:length(slams)
-        sB = subgradient(B,slams(i));
-        subObj(i) = getObj(Y,sB,slams(i));
-        [mcB,~] = mc(B,Y,slams(i));
-        mcObj(i) = getObj(Y,mcB,slams(i));
+        [sBCell{i},subObjCell{i}] = subgradient(B,Y,slams(i));
+        [mcBCell{i},mcObjCell{i}] = mc2(B,Y,slams(i));
+        fprintf('Finished part d, lam=%g\n',slams(i));
     end
 
-    save('TEST_RUN_03');
-
-    %load('TEST_RUN_03');
-    %% plotting
-    %figure, plot(rmseVec_train);
-    %hold on; plot(rmseVec_test,'r');
-    %figure, plot(cntVec);
-    %hold on; plot(rcntVec,'g');
-    %figure, plot(subObj-min(subObj));
-    %hold on; plot(mcObj-min(mcObj),'m');
-
-
+    % plotting
+    figure, plot(log(lams),rmseVec_train);
+    hold on; plot(log(lams),rmseVec_test,'r');
+    legend('training error','testing error');
+    xlabel('log(lambda)'); ylabel('RMSE');
+    %
+    figure, plot(log(lams),cntVec);
+    hold on; plot(log(rlams),rcntVec,'g');
+    legend('cool start','warm start');
+    xlabel('log(lambda)'); ylabel('number of iterations');
+    %
+    for i=1:length(subObjCell)
+        fmin = min(min(subObjCell{i}),min(mcObjCell{i}));
+        figure, plot(log(subObjCell{i}-fmin));
+        hold on; plot(log(mcObjCell{i}-fmin),'m');
+        legend('subgradient method','soft-impute');
+        xlabel('iteration'); ylabel('log(objective value)');
+    end
 end
 
 function [B,iterCnt] = mc(B,Y,lam)
@@ -62,9 +65,9 @@ function [B,iterCnt] = mc(B,Y,lam)
         [B,tnormB] = matSoftThresh(Y+Bno,lam);
         Bon = B; Bon(yno) = 0;
         obj = (1/2)*norm(Y-Bon,'fro')^2 + lam*tnormB;
-        %if (abs(obj-lobj)/lobj) < stopping
-            %break;
-        %end
+        if (abs(obj-lobj)/lobj) < stopping
+            break;
+        end
         lobj = obj;
         iterCnt = iterCnt+1;
     end
@@ -87,20 +90,33 @@ function err = rmse(Y,B)
     err = norm(errMat,'fro') / sqrt(size(yon,1));
 end
 
-function B = subgradient(B,lam)
+function [B,objVec] = subgradient(B,Y,lam)
     yno = find(Y==0);
     for i=1:500
         t = 1/i;
         Bon = B; B(yno) = 0;
-        subgrad = (Y-Bon) + lam%%%%%;
+        [U,S,V] = svd(B);
+        subgrad = (-2*Y+Bon) + lam*U*V';
         B = B - t*subgrad;
+        objVec(i) = getObj(Y,B,lam);
+    end
+end
+
+function [B,objVec] = mc2(B,Y,lam)
+    maxIter = 500;
+    yon = find(Y>0); yno = find(Y==0);
+    for i=1:maxIter
+        Bno = B; Bno(yon) = 0;
+        [B,tnormB] = matSoftThresh(Y+Bno,lam);
+        Bon = B; Bon(yno) = 0;
+        objVec(i) = (1/2)*norm(Y-Bon,'fro')^2 + lam*tnormB;
     end
 end
 
 function obj = getObj(Y,B,lam)
     yno = find(Y==0);
-    B(yno) = 0;
     [U,S,V] = svd(B);
     tnormB = sum(diag(S));
+    B(yno) = 0;
     obj = (1/2)*norm(Y-B,'fro')^2 + lam*tnormB;
 end
--- hw2/src/p5.m
+++ hw2/src/p5.m
@@ -6,22 +6,74 @@ function p5(trainWs,testWs)
     lams = logspace(0,3,30);
     B = zeros(size(Y));
     for i=1:length(lams)
-        %[Bcell{i},cntVec(i)] = mc(B,Y,lams(i));
+        [Bcell{i},cntVec(i)] = mc(B,Y,lams(i));
         [accBcell{i},accCntVec(i)] = accmc(B,Y,lams(i));
         fprintf('Finished part a, lam=%g, iterCnt=%d\n',lams(i),accCntVec(i));
     end
 
-    save('TEST_RUN_01_p5');
+    lam = 10;
+    [~,mcObjVec] = mc2(B,Y,lam);
+    [~,accmcObjVec] = accmc2(B,Y,lam);
 
-    %% part b
-    %rlams = fliplr(lams);
-    %[rBcell{1},rcntVec(1)] = mc(B,Y,rlams(1));
-    %fprintf('Finished part c, lam=%g, iterCnt=%d\n',rlams(1),rcntVec(1));
-    %for i=2:length(rlams)
-        %[rBcell{i},rcntVec(i)] = mc(rBcell{i-1},Y,rlams(i));
-        %[raccBcell{i},raccCntVec(i)] = accmc(raccBcell{i-1},Y,rlams(i));
-        %fprintf('Finished part c, lam=%g, iterCnt=%d\n',rlams(i),rcntVec(i));
-    %end
+    % part b
+    rlams = fliplr(lams);
+    [rBcell{1},rcntVec(1)] = mc(B,Y,rlams(1));
+    [raccBcell{1},raccCntVec(1)] = accmc(B,Y,rlams(1));
+    fprintf('Finished part b, lam=%g, accIterCnt=%d\n',rlams(1),raccCntVec(1));
+    for i=2:length(rlams)
+        [rBcell{i},rcntVec(i)] = mc(rBcell{i-1},Y,rlams(i));
+        [raccBcell{i},raccCntVec(i)] = accmc(raccBcell{i-1},Y,rlams(i));
+        fprintf('Finished part c, lam=%g, accIterCnt=%d\n',rlams(i),raccCntVec(i));
+    end
+
+    % part c
+    mona = im2double(rgb2gray(imread('mona_bw.jpg')));
+    B = zeros(size(mona));
+    mona1 = subsampImg(mona,0.5);
+    mona2 = subsampImg(mona,0.2);
+    mlams = [1e2,2e2,5e2];
+    B = zeros(size(mona));
+
+    for i=1:length(mlams)
+        [m1accBcell{i},~] = mc(B,mona1,mlams(i));
+        fprintf('Finished part c (0.5 subsamp), lam=%g\n',mlams(i));
+    end
+    for i=1:length(mlams)
+        [m2accBcell{i},~] = mc(B,mona2,mlams(i));
+        fprintf('Finished part c (0.2 subsamp), lam=%g\n',mlams(i));
+    end
+
+    % plotting
+    % a
+    figure, plot(lams,cntVec);
+    hold on; plot(lams,accCntVec,'r');
+    legend('soft-impute','accelerated-soft-impute');
+    xlabel('lambda'); ylabel('number of iterations');
+    xlim([0,300]);
+    %
+    figure, plot(mcObjVec);
+    hold on; plot(accmcObjVec,'r');
+    legend('soft-impute','accelerated-soft-impute');
+    xlabel('iteration'); ylabel('objective value');
+    xlim([0,300]);
+    % b
+    figure, plot(rlams,rcntVec);
+    hold on; plot(rlams,raccCntVec,'r');
+    legend('soft-impute (warm start)','accelerated-soft-impute (warm start)');
+    xlabel('lambda'); ylabel('number of iterations');
+    xlim([0,300]);
+    % c
+    figure, imagesc(mona);
+    figure, imagesc(mona1);
+    figure, imagesc(mona2);
+    plamsInd = [1,2,3];
+    for i=1:length(plamsInd)
+        figure, imagesc(m1accBcell{plamsInd(i)});
+    end
+    for i=1:length(plamsInd)
+        figure, imagesc(m2accBcell{plamsInd(i)});
+    end
+
 end
 
 function [B,iterCnt] = mc(B,Y,lam)
@@ -35,6 +87,7 @@ function [B,iterCnt] = mc(B,Y,lam)
         [B,tnormB] = matSoftThresh(Y+Bno,lam);
         Bon = B; Bon(yno) = 0;
         obj = (1/2)*norm(Y-Bon,'fro')^2 + lam*tnormB;
+        fprintf('mc, iter=%d, obj=%g\n',i,obj);
         if (abs(obj-lobj)/lobj) < stopping
             break;
         end
@@ -57,6 +110,7 @@ function [B2,iterCnt] = accmc(B1,Y,lam)
         [B2,tnormB] = matSoftThresh(Y+Bno,lam);
         Bon = B2; Bon(yno) = 0;
         obj = (1/2)*norm(Y-Bon,'fro')^2 + lam*tnormB;
+        fprintf('accmc, iter=%d, obj=%g\n',i,obj);
         if (abs(obj-lobj)/lobj) < stopping
             break;
         end
@@ -82,13 +136,55 @@ function err = rmse(Y,B)
     err = norm(errMat,'fro') / sqrt(size(yon,1));
 end
 
-function B = subgradient(B)
+function [B,objVec] = subgradient(B,Y,lam)
+    yno = find(Y==0);
+    for i=1:500
+        t = 1/i;
+        Bon = B; B(yno) = 0;
+        [U,S,V] = svd(B);
+        subgrad = (-2*Y+Bon) + lam*U*V';
+        B = B - t*subgrad;
+        objVec(i) = getObj(Y,B,lam);
+    end
+end
+
+function [B,objVec] = mc2(B,Y,lam)
+    maxIter = 500;
+    yon = find(Y>0); yno = find(Y==0);
+    for i=1:maxIter
+        Bno = B; Bno(yon) = 0;
+        [B,tnormB] = matSoftThresh(Y+Bno,lam);
+        Bon = B; Bon(yno) = 0;
+        objVec(i) = (1/2)*norm(Y-Bon,'fro')^2 + lam*tnormB;
+    end
+end
+
+function [B2,objVec] = accmc2(B1,Y,lam)
+    maxIter = 500;
+    yon = find(Y>0); yno = find(Y==0);
+    iterCnt = 0;
+    B2 = B1;
+    for i=3:maxIter
+        B = B2 + ((i-2)/(i+1))*(B2 - B1);
+        B1 = B2;
+        Bno = B; Bno(yon) = 0;
+        [B2,tnormB] = matSoftThresh(Y+Bno,lam);
+        Bon = B2; Bon(yno) = 0;
+        objVec(i) = (1/2)*norm(Y-Bon,'fro')^2 + lam*tnormB;
+        iterCnt = iterCnt+1;
+    end
 end
 
 function obj = getObj(Y,B,lam)
     yno = find(Y==0);
-    B(yno) = 0;
     [U,S,V] = svd(B);
     tnormB = sum(diag(S));
+    B(yno) = 0;
     obj = (1/2)*norm(Y-B,'fro')^2 + lam*tnormB;
+end
+
+function I = subsampImg(I,frac)
+    numoffpts = (1-frac)*length(I(:));
+    offpts = randperm(length(I(:)),numoffpts);
+    I(offpts) = 0;
 end
